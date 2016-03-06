7757354589d6b44e55abc0a302fbe04fe52684d2
Rattanak Chea <rattanak.chea@gmail.com>
2016-03-05 22:42:40 -0800

    redesign layout, combine grunt file

--- gruntfile.js
+++ gruntfile.js
@@ -1,18 +1,96 @@
-var pkgjson = require('./package.json');
-
-var config = {
-    pkg: pkgjson,
-    app: 'app',
-    dist: 'dist'
-}
-
 module.exports = function(grunt) {
 
-    // Configuration
     grunt.initConfig({
-        config: config,
-        pkg: config.pkg,
-        bower: { directory: 'dist' },
+
+        //read from package.json to get its name value pair
+        pkg: grunt.file.readJSON('package.json'),
+
+
+        concat: {
+            options: {
+                separator: '\n//========end of a file======\n\n'
+            },
+            dist: {
+                src: ['app/js/data.js', 'app/js/app.js', 'app/js/controllers/*.js'],
+                dest: 'dist/js/<%= pkg.name %>.js'
+            }
+        },
+        bower: {
+            install: {
+                options: {
+                    install: true,
+                    copy: false,
+                    targetDir: './libs',
+                    cleanTargetDir: true
+                }
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/js/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+        jshint: {
+            files: ['Gruntfile.js', 'app/**/*.js'],
+            options: {
+                // options here to override JSHint defaults
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true,
+                    document: true
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['**/*.js'],
+                tasks: ['concat'],
+            },
+           options: {
+                  spawn: false,
+                  livereload: true
+            }
+        },
+
+        connect: {
+            server: {
+                options: {
+                    port: 8000,
+                    base: '.',
+                    keepalive: true,
+                    hostname: 'localhost',
+                    livereload: true,
+                    open: true
+                }
+            }
+        },
+
+        //wiredep
+        wiredep: {
+            task: {
+                src: [
+                    'app/index.html',
+                    'app/scripts/*.js'
+                ]
+            }
+        },
+
+        //for generating dist folder
+        bower_concat: {
+            all: {
+                dest: {
+                    js: 'dist/_bower.js',
+                    css: 'dist/_bower.css'
+                }
+            }
+        },
+
         copy: {
             main: {
                 expand: true,
@@ -20,23 +98,23 @@ module.exports = function(grunt) {
                 src: '**',
                 dest: 'dist/'
             }
-        },
-
-        bower_concat: {
-            all: {
-                dest: {
-                    js: 'dist/bower.js',
-                    css: 'dist/bower.css'
-                }
-            }
         }
+
     });
 
-    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    //grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-wiredep');
     grunt.loadNpmTasks('grunt-bower-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask('serve', [
-        'copy'
-    ]);
+
+    grunt.registerTask('test', ['jshint']);
+
+    grunt.registerTask('default', ['wiredep', 'connect', 'watch']);
+
 };
---
+++ gruntfile0.js
@@ -0,0 +1,42 @@
+var pkgjson = require('./package.json');
+
+var config = {
+    pkg: pkgjson,
+    app: 'app',
+    dist: 'dist'
+}
+
+module.exports = function(grunt) {
+
+    // Configuration
+    grunt.initConfig({
+        config: config,
+        pkg: config.pkg,
+        bower: { directory: 'dist' },
+        copy: {
+            main: {
+                expand: true,
+                cwd: 'app',
+                src: '**',
+                dest: 'dist/'
+            }
+        },
+
+        bower_concat: {
+            all: {
+                dest: {
+                    js: 'dist/bower.js',
+                    css: 'dist/bower.css'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-bower-concat');
+
+    grunt.registerTask('serve', [
+        'copy'
+    ]);
+};
